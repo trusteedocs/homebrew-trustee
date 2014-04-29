2f16e466dc1452525361019932f81fe1ef26ab96
David Dinh <david@dinh.io>
2014-04-28 19:02:30 -0700

    Add Trustee.app v1.0.0

---
+++ Casks/trustee.rb
@@ -0,0 +1,7 @@
+class Trustee < Cask
+  url 'https://github.com/trusteedocs/trustee/releases/download/v1.0/Trustee-1.0.0.app.zip'
+  homepage 'https://github.com/trusteedocs/trustee'
+  version '1.0.0'
+  sha256 '37a6101554d6134c85e89f389cd60b749f315efc7aa024c1a8f28015a31e4e9f'
+  link 'Trustee.app'
+end
